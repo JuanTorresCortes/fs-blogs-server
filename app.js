641091a1b84479add2ea8bdfb2816497a4c182dd
Juan <juan.giana8589@gmail.com>
2023-07-08 19:15:50 -0500

    added modle

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Import required dependencies
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require("cors")
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const cors = require("cors");
 require("dotenv").config();
 
 // Connect to MongoDB
@@ -12,43 +13,43 @@ const { mongooseConnect } = require("./db");
 mongooseConnect();
 
 // Import routers
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 const blogsRouter = require("./routes/blogs");
 
 var app = express();
 
 // Set up view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middleware
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route handlers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/blogs', blogsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/blogs", blogsRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- model/Blog.js
+++ model/Blog.js
@@ -5,8 +5,9 @@ const { v4: uuid } = require("uuid");
 const blogsSchema = new mongoose.Schema({
   _id: { type: String, default: uuid }, // Unique ID generated using uuid
   title: { type: String, required: true }, // Title of the blog (required)
+  author: { type: String, required: true},
   content: { type: String, required: true }, // Content of the blog (required)
-  lastModified: { type: Date, default: Date.now }, // Timestamp for last modification (default: current date/time)
+  lastModified: { type: Date, default: Date.now , required: true}, // Timestamp for last modification (default: current date/time)
 });
 
 // Create the Blog model using the blogs schema
