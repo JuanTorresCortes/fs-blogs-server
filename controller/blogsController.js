f9ddba7d1484054e6a9c4597a848d5417301cfad
Juan <juan.giana8589@gmail.com>
2023-07-11 09:35:49 -0500

    added edit and delete routs

--- controller/blogsController.js
+++ controller/blogsController.js
@@ -33,7 +33,41 @@ const allBlogs = async (req, res) => {
   }
 };
 
+const editBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    req.body.lastModified = Date.now();
+
+    // Find the blog by ID in the database
+    const updateBlog = await Blog.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+
+    if (!updateBlog) {
+      return res.status(400).json({ message: "Blog not found" });
+    }
+    res.status(200).json({ success: true, data: updateBlog });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBlog = await Blog.findOneAndDelete({ _id: id });
+    res.status(200).json({ success: true, data: deletedBlog });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createBlog,
   allBlogs,
+  editBlog,
+  deleteBlog,
 };
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createBlog, allBlogs } = require("../controller/blogsController");
+const {
+  createBlog,
+  allBlogs,
+  editBlog,
+  deleteBlog,
+} = require("../controller/blogsController");
 
 // Endpoint to return a simple "hello" message
 router.get("/hello", (req, res) => {
@@ -12,5 +17,10 @@ router.get("/all-blogs", allBlogs);
 
 // Endpoint to create a new blog
 router.post("/new-blog", createBlog);
+
+// Endpoint to retrieve blog by id
+router.put("/edit/:id", editBlog);
+
+router.delete("/delete/:id", deleteBlog);
 
 module.exports = router;
